69799a5c1abee028abd5688cdd52ff68e08bf0cb
jseiden <jseiden@gmail.com>
2015-08-14 19:16:03 -0700

    Word-Count

---
+++ Easy/12.Vowel-Count.js
@@ -0,0 +1,11 @@
+//Using the JavaScript language, have the function VowelCount(str) take the str string parameter being passed and return the number of vowels the string contains (ie. "All cows eat grass" would return 5). Do not count y as a vowel for this challenge. 
+
+function VowelCount(str) {
+  var count = 0;
+  str.split("").forEach(function(letter) {
+    if(letter.match(/[aeiou]/i)) {
+     count++;
+    }
+  });
+  return count;
+}
