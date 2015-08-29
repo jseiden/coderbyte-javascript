f6a0e0f47c4aa888a9bc2a5cd13863210ea11b75
jseiden <jseiden@gmail.com>
2015-08-29 03:38:36 -0700

    Number-Addition

---
+++ Easy/25.Number-Addition.js
@@ -0,0 +1,14 @@
+// Using the JavaScript language, have the function NumberSearch(str) take the str parameter, 
+// search for all the numbers in the string, add them together, then return that final number. 
+// For example: if str is "88Hello 3World!" the output should be 91. 
+// You will have to differentiate between single digit numbers and multiple digit numbers like in the example above. 
+// So "55Hello" and "5Hello 5" should return two different answers. Each string will contain at least one letter or symbo
+
+function NumberAddition(str) {
+   var arr = str.split(/[^0-9]/); 
+   return arr.reduce(function(a,b){
+     return +a + +b}
+   );
+}
+
+
