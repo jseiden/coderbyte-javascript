eca7b41cb63ff533f4a3743700bc0a5012d88eff
jseiden <jseiden@gmail.com>
2015-08-31 03:34:58 -0700

    Off-Line-Minimum

---
+++ Easy/30.Off-Line-Minimum.js
@@ -0,0 +1,20 @@
+// Using the JavaScript language, have the function OffLineMinimum(strArr) take the strArr parameter being passed 
+// which will be an array of integers ranging from 1...n and the letter "E" and return the correct subset based on the following rules. 
+// The input will be in the following format: ["I","I","E","I",...,"E",...,"I"] where the I's stand for integers 
+// and the E means take out the smallest integer currently in the whole set. 
+// When finished, your program should return that new set with integers separated by commas. 
+// For example: if strArr is ["5","4","6","E","1","7","E","E","3","2"] then your program should return 4,1,5.
+
+function OffLineMinimum(strArr) {
+  var currentArr = [];
+  var results = [];
+  for(var i = 0; i < strArr.length;i++) {
+    if(!isNaN(strArr[i])) {
+      currentArr.push(strArr[i]);
+      currentArr.sort(function(a,b){return a - b})
+    } else if(strArr[i] === "E") {
+      results.push((currentArr.shift()));
+    }
+  }
+  return results.join(",").toString();
+}
