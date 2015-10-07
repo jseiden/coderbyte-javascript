88f5f41bcc08eda8b0d09c8762b1d2771b41b10a
jseiden <jseiden@gmail.com>
2015-10-06 20:48:18 -0700

    minor change

--- Easy/18.Array-Addition-I.js
+++ Easy/18.Array-Addition-I.js
@@ -1,5 +1,6 @@
 // Using the JavaScript language, have the function ArrayAdditionI(arr) take the array of numbers stored in arr 
-// and return the string true if any combination of numbers in the array can be added up to equal the largest number in the array, otherwise return the string false. 
+// and return the string true if any combination of numbers in the array can be added up to equal
+// the largest number in the array, otherwise return the string false. 
 // For example: if arr contains [4, 6, 23, 10, 1, 3] the output should return true because 4 + 6 + 10 + 3 = 23. 
 // The array will not be empty, will not contain all the same elements, and may contain negative numbers. 
 
@@ -26,7 +27,6 @@ function ArrayAdditionI(arr) {
   var result = false;
 
   var findCombo = function(sum, i) {
-    var temp = [];
     sum = sum || 0;
     i = i || 0;
     if(sum === largestNum) {
