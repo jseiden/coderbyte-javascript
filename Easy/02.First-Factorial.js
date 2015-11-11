0805e37f10d292a43be88243338fc1c1336bebd7
jseiden <jseiden@gmail.com>
2015-11-11 05:47:51 -0800

    First-Factorial recursive version

--- Easy/02.First-Factorial.js
+++ Easy/02.First-Factorial.js
@@ -1,10 +1,16 @@
 // Using the JavaScript language, have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it 
 // (ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18. 
 
-function FirstFactorial(num) { 
-  var total = 1;
-  for(var i = num; i > 0; i--) {
-      total = total * i;
-    }
-  return total;     
+//function FirstFactorial(num) { 
+  //var total = 1;
+  //for(var i = num; i > 0; i--) {
+      //total = total * i;
+    //}
+  //return total;     
+//}
+
+function FirstFactorial(num) {
+  return num < 2 ? 1 : num * FirstFactorial(num - 1);
 }
+
+
