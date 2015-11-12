346e49a74e19fd9313ca9a7a3a332949407b3e8c
jseiden <jseiden@gmail.com>
2015-11-11 23:06:14 -0800

    AB-Check cleanup

--- Easy/11.AB-Check.js
+++ Easy/11.AB-Check.js
@@ -4,12 +4,10 @@
 
 function ABCheck(str) {
   var arr = str.split("");
-  console.log(arr);
+
   for(var i = 0; i < arr.length; i++) {
     if(arr[i] === "a") {
       for(var j = 0 ; j < arr.length; j++) {
-        console.log(arr[j]);
-        console.log(j + "-" + i + ":" + Math.abs(j-i));
         if(arr[j] === "b" && Math.abs(j - i) === 4) {
           return "true";
         }
@@ -17,6 +15,7 @@ function ABCheck(str) {
 
     }
   }
-  return false;
 
+  return false;
 }
+
