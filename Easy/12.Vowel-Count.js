3b69e617aa121a6c919ac8737aa0c5d5db979d61
jseiden <jseiden@gmail.com>
2015-11-11 23:15:59 -0800

    Vower-Count cleanup

--- Easy/12.Vowel-Count.js
+++ Easy/12.Vowel-Count.js
@@ -2,12 +2,19 @@
 // and return the number of vowels the string contains
 // (ie. "All cows eat grass" would return 5). Do not count y as a vowel for this challenge. 
 
+//function VowelCount(str) {
+  //var count = 0;
+  //str.split("").forEach(function(letter) {
+    //if(letter.match(/[aeiou]/i)) {
+     //count++;
+    //}
+  //});
+  //return count;
+//}
+
 function VowelCount(str) {
-  var count = 0;
-  str.split("").forEach(function(letter) {
-    if(letter.match(/[aeiou]/i)) {
-     count++;
-    }
-  });
-  return count;
+  return str.split("").filter(function (letter) { 
+    return letter.match(/[aeiou]/i);
+  }).length;
 }
+
