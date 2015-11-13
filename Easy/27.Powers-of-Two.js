1b537a43769d2979886a7f78c832f6cdb15e7a88
jseiden <jseiden@gmail.com>
2015-11-12 22:18:27 -0800

    Powers-Of-Two recursive cleanup

--- Easy/27.Powers-of-Two.js
+++ Easy/27.Powers-of-Two.js
@@ -3,11 +3,16 @@
 // If it's not return the string false. For example if the input is 16 then your program should return the string true 
 // but if the input is 22 then the output should be the string false. 
 
+//function PowersofTwo(num) {
+  //test = num;
+  //while(test > 2){
+    //test = test/2;
+  //}
+  //return test === 2 ? true : false;
+//}
+
 function PowersofTwo(num) {
-  test = num;
-  while(test > 2){
-    test = test/2;
-  }
-  return test === 2 ? true : false;
+  return num === 2 ? "true" : num < 2 ? "false" : PowersofTwo(num/2);
 }
+
 
