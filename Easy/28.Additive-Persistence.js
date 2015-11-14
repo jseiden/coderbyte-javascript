996579f54a8be97da5885bbd0f69736b4bfd3819
jseiden <jseiden@gmail.com>
2015-11-14 01:19:44 -0800

    Additive-Persistence cleanup

--- Easy/28.Additive-Persistence.js
+++ Easy/28.Additive-Persistence.js
@@ -3,18 +3,34 @@
 // which is the number of times you must add the digits in num until you reach a single digit. 
 // For example: if num is 2718 then your program should return 2 because 2 + 7 + 1 + 8 = 18 and 1 + 8 = 9 and you stop at 9. 
 
+//function AdditivePersistence(num) {
+  //var count = 0;
+
+  //function addAll(n){
+    //return n.toString().split("").reduce(function(a,b){return +a + +b});
+  //}
+  
+  //while(num.toString().length > 1) {
+    //count++;
+    //num = addAll(num);
+  //}
+
+  //return count;
+
+//}
+
 function AdditivePersistence(num) {
   var count = 0;
 
   function addAll(n){
-    return n.toString().split("").reduce(function(a,b){return +a + +b});
-  }
-  
-  while(num.toString().length > 1) {
+    if (n.toString().length === 1) {
+      return;
+    }
     count++;
-    num = addAll(num);
+    addAll(n.toString().split("").reduce(function(a,b){return +a + +b}));
   }
+  addAll(num);
 
   return count;
-
 }
+
