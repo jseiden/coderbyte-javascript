2ac57fb26f7c5ec443f36b65720e16da86a0ec9a
jseiden <jseiden@gmail.com>
2015-11-17 06:15:25 -0800

    make First-Reverse recursive

--- Easy/01.First-Reverse.js
+++ Easy/01.First-Reverse.js
@@ -1,5 +1,6 @@
 // Using the JavaScript language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
 
 function FirstReverse(str) {
-  return str.split("").reverse().join("");
+  return str === "" ? "" : FirstReverse(str.slice(1)) + str[0];
 }
+
