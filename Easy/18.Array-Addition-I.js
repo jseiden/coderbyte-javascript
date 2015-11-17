527c94eb26926636563f39c6a4be523e799b41fb
jseiden <jseiden@gmail.com>
2015-11-17 00:14:25 -0800

    Array-Addition cleanup

--- Easy/18.Array-Addition-I.js
+++ Easy/18.Array-Addition-I.js
@@ -4,25 +4,8 @@
 // For example: if arr contains [4, 6, 23, 10, 1, 3] the output should return true because 4 + 6 + 10 + 3 = 23. 
 // The array will not be empty, will not contain all the same elements, and may contain negative numbers. 
 
- //function ArrayAdditionI(arr) {
-  //var sortedArr = arr.sort(function(a,b){ return a - b });
-  //var largestNum = arr.pop();
-  //var findCombo = function(target, array) {
-    //if(array.length === 0) {
-       ////returns false if array is empty and target is not 0. this means there
-       ////was a remainder and that no numbers go in evenly 
-      //return target === 0;
-    //}
-    //var n = array[0];
-    //array = array.slice(1);
-    
-    //return findCombo(target, array) || findCombo(target - n, array);
-  //}
-  //return findCombo(largestNum, arr);
-//}
-
 function ArrayAdditionI(arr) {
-  var sortedArr = arr.sort(function(a,b){return a - b});
+  var sortedArr = arr.sort(function(a,b){ return a - b; });
   var largestNum = arr.pop();
   var result = false;
 
@@ -39,7 +22,9 @@ function ArrayAdditionI(arr) {
       sum -= arr[i];
     }
   };
+
   findCombo();
+
   return result;
 }
-
+ 
