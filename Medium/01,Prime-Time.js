b6dfffb0fde642c577685ff4fdbc20f7265ea224
jseiden <jseiden@gmail.com>
2015-11-23 23:55:35 -0800

    stray line

--- Medium/01,Prime-Time.js
+++ Medium/01,Prime-Time.js
@@ -1,4 +1,3 @@
-
 // Using the JavaScript language, have the function PrimeTime(num) take the num parameter being passed and return the string true if the parameter is a prime number, 
 // otherwise return the string false. The range will be between 1 and 2^16. 
 
